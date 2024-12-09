2df715b89096b954ce3019c4bfa85cb11d0b4f8e
yummyr <ttang1873@gmail.com>
2024-12-09 09:38:07 -0800

    initial commit

---
+++ src/common/local-data.js
@@ -0,0 +1,127 @@
+export const headerLinks = [
+  {
+    title: "Find Music",
+    link: "/"
+  },
+  {
+    title: "My Music",
+    link: "/mine"
+  },
+  {
+    title: "Friend",
+    link: "/friend"
+  },
+  {
+    title: "Store",
+    link: "https://music.163.com/store/product"
+  },
+  {
+    title: "Musician",
+    link: "https://music.163.com/nmusician/web/index#/"
+  },
+  {
+    title: "Download",
+    link: "https://music.163.com/#/download"
+  }
+]
+
+
+export const footerLinks = [
+  {
+    title: "Service",
+    link: "https://st.music.163.com/official-terms/service"
+  },
+  {
+    title: "Privacy",
+    link: "https://st.music.163.com/official-terms/privacy"
+  },
+  {
+    title: "Children",
+    link: "https://st.music.163.com/official-terms/children"
+  },
+  {
+    title: "Complaints",
+    link: "https://music.163.com/st/staticdeal/complaints.html"
+  },
+  {
+    title: "Feedback",
+    link: "#"
+  }
+]
+
+export const footerImages = [
+  {
+    link: "https://music.163.com/st/userbasic#/auth"
+  },
+  {
+    link: "https://music.163.com/recruit"
+  },
+  {
+    link: "https://music.163.com/web/reward"
+  },
+  {
+    link: "https://music.163.com/uservideo#/plan"
+  }
+]
+
+// discover中的数据
+export const dicoverMenu = [
+  {
+    title: "Recommend",
+    link: "/recommend"
+  },
+  {
+    title: "Ranking",
+    link: "/ranking"
+  },
+  {
+    title: "Songs",
+    link: "/songs"
+  },
+  {
+    title: "Dj",
+    link: "/djradio"
+  },
+  {
+    title: "Artist",
+    link: "/artist"
+  },
+  {
+    title: "Album",
+    link: "/album"
+  },
+]
+
+
+export const hotRadios = [
+  {
+      picUrl: "http://p1.music.126.net/H3QxWdf0eUiwmhJvA4vrMQ==/1407374893913311.jpg",
+      name:"Li Chen",
+      position:"Psychologist and gourmet Professor Chen Li",
+      url:"/user/home?id=278438485"
+  },
+  {
+      picUrl: "http://p1.music.126.net/y5-sM7tjnxnu_V9LWKgZlw==/7942872001461517.jpg",
+      name:"DJ Yan Qiu",
+      position:"Famous music program host",
+      url:"/user/home?id=91239965",
+  },
+  {
+      picUrl: "http://p1.music.126.net/6cc6lgOfQTo6ovNnTHPyJg==/3427177769086282.jpg",
+      name:"National Centre for the Performing Arts Classical Music Channel",
+      position:"National Centre for the Performing Arts Classical Music Official",
+      url:"/user/home?id=324314596",
+  },
+  {
+      picUrl:  "http://p1.music.126.net/xa1Uxrrn4J0pm_PJwkGYvw==/3130309604335651.jpg",
+      name:"Thanks for listening",
+      position:"Nanjing Radio host Wang Xin",
+      url:"/user/home?id=1611157",
+  },
+  {
+      picUrl: "http://p1.music.126.net/slpd09Tf5Ju82Mv-h8MP4w==/3440371884651965.jpg",
+      name:"DJ Xiao Su",
+      position:"Independent DJ, guest DJ for CRI Global Travel Radio",
+      url:"/user/home?id=2313954"
+  }
+]
